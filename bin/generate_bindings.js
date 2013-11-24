4b60fbc83d594857e8f55fc439b2d30860d338a9
Pras Velagapudi <psigen@gmail.com>
2013-11-24 02:14:31 -0500

    Added proper generate script, tried to integrate into build system.

---
+++ bin/generate_bindings.js
@@ -0,0 +1,24 @@
+/**
+ * This script automatically generates bindings for the Bullet C API.
+ */
+var path = require('path');
+var fs = require('fs');
+var jsb = require('js-beautify');
+var generate = require('ffi-generate').generate;
+
+// Automatically generate bullet bindings
+var result = generate({
+    filename: path.join(__dirname, '..', 'src', 'bullet', 'src', 'Bullet-C-Api.h'),
+    library: path.join(__dirname, '..', 'build', 'Release', 'libbullet.so')
+});
+
+// Report functions that could not be mapped
+if (result.unmapped.length > 0) {
+    console.log('----- UNMAPPED FUNCTIONS -----');
+    console.log(result.unmapped);
+    console.log('----- UNMAPPED FUNCTIONS -----');
+}
+
+// Write the bindings to file
+fs.writeFileSync(path.join(__dirname, '..', 'lib', 'bulletphysics.js'), 
+                 jsb.js_beautify(result.serialized));
